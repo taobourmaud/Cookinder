b46440ce277d36fa8d6e9584314c53232872b6a4
Louis Teilliais <louis.teilliais@gmail.com>
2025-02-25 08:41:08 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { supabase } from './supabase';
 
 export default function App() {
+
+
+  async function testSupabase() {
+    const { data, error } = await supabase
+      .from('test')
+      .select('*') 
+    console.log(data)
+  }
+
+  testSupabase()
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://mlrkhwcpysblsffmgctw.supabase.co/"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1scmtod2NweXNibHNmZm1nY3R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MDE5MjksImV4cCI6MjA1NTk3NzkyOX0.gBg3b2o93U1uLndUUHLR3L6j0oXSBj-0yfaHEO9lU6Y"
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || '';
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and Anon Key must be provided');
+}
+
+console.log(process.env.SUPABASE_URL)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
