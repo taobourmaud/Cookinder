79186f9ed6b1288faf699b35e32e1ee8e860ba9b
taobourmaud <tao.bourmaud@ynov.com>
2025-03-17 09:08:36 +0100

    style for camera

--- src/screens/takePicture.screen.tsx
+++ src/screens/takePicture.screen.tsx
@@ -1,7 +1,6 @@
 import { CameraView, Camera } from "expo-camera";
 import { useState, useRef, useEffect } from "react";
 import {
-  Button,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -14,74 +13,57 @@ import { useNavigation } from "@react-navigation/native";
 import * as MediaLibrary from "expo-media-library";
 
 export default function CameraFunction() {
-  const [cameraPermission, setCameraPermission] = useState(); //State variable for camera permission
-  const [mediaLibraryPermission, setMediaLibraryPermission] = useState(); //State variable for media library permission
-  const [micPermission, setMicPermission] = useState(); //// state variable for microphone permission
-  const [cameraMode, setCameraMode] = useState("picture"); //State variable for picture or video. By default it will be for picture
+  const [cameraPermission, setCameraPermission] = useState();
+  const [mediaLibraryPermission, setMediaLibraryPermission] = useState();
   const [facing, setFacing] = useState("back");
-  const [photo, setPhoto] = useState(); //After picture is taken this state will be updated with the picture
-  const [video, setVideo] = useState(); //After video is recorded this state will be updated
-  const [flashMode, setFlashMode] = useState("on"); //Camera Flash will be ON by default
-  const [recording, setRecording] = useState(false); //State will be true when the camera will be recording
-  const [zoom, setZoom] = useState(0); //State to control the digital zoom
-  let cameraRef = useRef(); //Creates a ref object and assigns it to the variable cameraRef.
+  const [photo, setPhoto] = useState();
+  let cameraRef = useRef();
   const navigation = useNavigation();
 
-  //When the screen is rendered initially the use effect hook will run and check if permission is granted to the app to access the Camera, Microphone and Media Library.
+  // Demande des permissions au chargement de la page
   useEffect(() => {
     (async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
       const mediaLibraryPermission =
         await MediaLibrary.requestPermissionsAsync();
-      const microphonePermission =
-        await Camera.requestMicrophonePermissionsAsync();
       setCameraPermission(cameraPermission.status === "granted");
       setMediaLibraryPermission(mediaLibraryPermission.status === "granted");
-      setMicPermission(microphonePermission.status === "granted");
     })();
   }, []);
 
-  //If permissions are not granted app will have to wait for permissions
+  // Gestion des permissions
   if (
     cameraPermission === undefined ||
-    mediaLibraryPermission === undefined ||
-    micPermission === undefined
+    mediaLibraryPermission === undefined
   ) {
-    return <Text>Request Permissions....</Text>;
+    return <Text style={styles.message}>Demande de permissions en cours...</Text>;
   } else if (!cameraPermission) {
     return (
-      <Text>
-        Permission for camera not granted. Please change this in settings
+      <Text style={styles.message}>
+        Permission pour la caméra non accordée. Veuillez modifier cela dans les paramètres.
       </Text>
     );
   }
 
-  //Function to toggle between back and front camera
+  // Fonction pour basculer entre caméra avant et arrière
   function toggleCameraFacing() {
     setFacing((current) => (current === "back" ? "front" : "back"));
   }
 
-  //Function to toggle flash on or off
-  function toggleFlash() {
-    setFlashMode((current) => (current === "on" ? "off" : "on"));
-  }
-
-  //Function to capture picture
+  // Fonction pour prendre une photo
   let takePic = async () => {
-    //Declares takePic as an asynchronous function using the async keyword.
     let options = {
-      quality: 1, //Specifies the quality of the captured image. A value of 1 indicates maximum quality, whereas lower values reduce quality (and file size).
-      base64: true, //Includes the image's Base64 representation in the returned object. This is useful for embedding the image directly in data URIs or for immediate upload to servers.
-      exif: false, //Disables the inclusion of EXIF metadata in the image (e.g., location, device info). Setting this to true would include such metadata.
+      quality: 1,
+      base64: true,
+      exif: false,
     };
 
-    let newPhoto = await cameraRef.current.takePictureAsync(options); //Refers to the camera instance (set using a ref in React). This is used to call methods on the camera.
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
     console.log("Photo prise :", newPhoto.uri);
-    //Captures an image with the specified options and returns a promise that resolves to an object containing: URI and Base64 string and/or EXIF data, based on the provided options.
-    setPhoto(newPhoto); //Update photo state with the new photo object
+    setPhoto(newPhoto);
   };
 
-  //After the picture is captured it will be displayed to the user and the user will also be provided the option to save or discard the image
+  // Affichage de la photo après capture avec options de sauvegarde ou de suppression
   if (photo) {
     let savePhoto = () => {
       navigation.navigate("PhotoForm", { imageUri: photo.uri });
@@ -93,94 +75,50 @@ export default function CameraFunction() {
         <View style={styles.btnContainer}>
           {mediaLibraryPermission ? (
             <TouchableOpacity style={styles.btn} onPress={savePhoto}>
-              <Ionicons name="save-outline" size={30} color="black" />
+              <Ionicons name="checkmark-circle-outline" size={50} color="#4CAF50" />
+              <Text style={styles.btnText}>Utiliser</Text>
             </TouchableOpacity>
           ) : undefined}
           <TouchableOpacity
             style={styles.btn}
             onPress={() => setPhoto(undefined)}
           >
-            <Ionicons name="trash-outline" size={30} color="black" />
+            <Ionicons name="close-circle-outline" size={50} color="#F44336" />
+            <Text style={styles.btnText}>Retour</Text>
           </TouchableOpacity>
         </View>
       </SafeAreaView>
     );
   }
 
-  //Video Recorder
-  async function recordVideo(){
-    setRecording(true); //Updates the recording state to true. This will also toggle record button to stop button.
-    cameraRef.current.recordAsync({ //cameraRef is a useRef hook pointing to the camera component. It provides access to the camera's methods, such as recordAsync. Starts recording a video and returns a Promise that resolves with the recorded video’s details.
-      maxDuration: 30, //Limits the recording duration to 30 seconds. After 30 seconds, the recording automatically stops, and the Promise resolves.
-    })
-    .then((newVideo) => { //The result of this Promise is an object (newVideo) containing information about the recorded video, such as the file's URI and other metadata. This callback runs when the recording completes successfully. 
-      setVideo(newVideo); // Stores the recorded video details in the state, which can later be used for playback, uploading, or other actions.
-      setRecording(false);
-    })
-    console.log(video.uri)
-  }
-
-  function stopRecording(){
-    setRecording(false);
-    cameraRef.current.stopRecording();
-    console.log("Recording stopped");
-  }
-
-  if(video) {
-    let uri = video.uri;
-    navigation.navigate("Video", {uri})
-  }
-  //We will design the camera UI first
+  // Interface de la caméra simplifiée
   return (
     <View style={styles.container}>
       <CameraView
         style={styles.camera}
         facing={facing}
         ref={cameraRef}
-        flash={flashMode}
-        mode={cameraMode}
-        zoom={zoom}
       >
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Ionicons name="camera-reverse-outline" size={20} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => setCameraMode("picture")}
-          >
-            <Ionicons name="camera-outline" size={20} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => setCameraMode("video")}
-          >
-            <Ionicons name="videocam-outline" size={20} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={toggleFlash}>
-            <Text>
-              {flashMode === "on" ? (
-                <Ionicons name="flash-outline" size={20} color="white" />
-              ) : (
-                <Ionicons name="flash-off-outline" size={20} color="white" />
-              )}
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.shutterContainer}>
-          {cameraMode === "picture" ? (
-            <TouchableOpacity style={styles.button} onPress={takePic}>
-              <Ionicons name="aperture-outline" size={40} color="white" />
+        <View style={styles.controlsContainer}>
+          <View style={styles.spacer} />
+          
+          <View style={styles.shutterContainer}>
+            <TouchableOpacity
+              style={styles.captureButton}
+              onPress={takePic}
+            >
+              <View style={styles.captureButtonInner} />
             </TouchableOpacity>
-          ) : recording ? (
-            <TouchableOpacity style={styles.button} onPress={stopRecording}>
-              <Ionicons name="stop-circle-outline" size={40} color="red" />
+          </View>
+          
+          <View style={styles.flipContainer}>
+            <TouchableOpacity
+              style={styles.flipButton}
+              onPress={toggleCameraFacing}
+            >
+              <Ionicons name="camera-reverse-outline" size={30} color="white" />
             </TouchableOpacity>
-          ) : (
-            <TouchableOpacity style={styles.button} onPress={recordVideo}>
-              <Ionicons name="play-circle-outline" size={40} color="white" />
-            </TouchableOpacity>
-          )}
+          </View>
         </View>
       </CameraView>
     </View>
@@ -190,56 +128,82 @@ export default function CameraFunction() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-  },
-  message: {
-    textAlign: "center",
-    paddingBottom: 10,
+    backgroundColor: "#000",
   },
   camera: {
     flex: 1,
   },
-  buttonContainer: {
-    flex: 1,
+  message: {
+    textAlign: "center",
+    padding: 20,
+    fontSize: 16,
+  },
+  controlsContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 120,
     flexDirection: "row",
-    backgroundColor: "transparent",
-    margin: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    paddingBottom: 20,
+  },
+  spacer: {
+    width: 60,
   },
   shutterContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 20,
-  },
-  button: {
     flex: 1,
-    alignSelf: "flex-end",
     alignItems: "center",
   },
-  btnContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    backgroundColor: "white",
-  },
-  btn: {
+  captureButton: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: "rgba(255, 255, 255, 0.3)",
     justifyContent: "center",
-    margin: 10,
-    elevation: 5,
+    alignItems: "center",
+  },
+  captureButtonInner: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "rgba(0, 0, 0, 0.1)",
+  },
+  flipContainer: {
+    width: 60,
+    alignItems: "center",
+  },
+  flipButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
-    height: "95%",
-    width: "100%",
+    flex: 1,
+    backgroundColor: "#000",
   },
   preview: {
-    alignSelf: "stretch",
     flex: 1,
-    width: "auto",
+    width: "100%",
   },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "white",
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 20,
+  },
+  btn: {
+    alignItems: "center",
+  },
+  btnText: {
+    marginTop: 5,
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
